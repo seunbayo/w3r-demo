da09f4e4d4b6a19e5b37177aba51a06d2c6cb2bf
seunbayo <seunbayo83@gmail.com>
2022-05-17 03:57:46 +0100

    added react injector

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div>
-      <button>Connect to MetaMask</button>
+      <button className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">
+        Connect to MetaMask
+      </button>
+      <span>Not connected</span>
     </div>
   );
 }
