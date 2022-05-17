207cafa280fabdf409e5712728a996cd28d1e221
seunbayo <seunbayo83@gmail.com>
2022-05-17 04:09:18 +0100

    added w3 and reactcore

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import { injected } from "../components/wallet/connectors"
+
 export default function Home() {
+function connect() {
+    
+  }
+
+
   return (
     <div>
-      <button className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">
+      <button onClick={connect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">
         Connect to MetaMask
       </button>
       <span>Not connected</span>
