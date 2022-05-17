883570e9bd38ab7ffd32c4b2bbbb6be7e757cf60
seunbayo <seunbayo83@gmail.com>
2022-05-17 05:04:18 +0100

    made changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import { web3ReactProvider } from '@web3-react/core'
+import web3 from 'web3'
+
+
+
+
+function getLibrary(provider) {
+  return new web3(provide)
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </web3ReactProvider>
+  );
 }
 
 export default MyApp
