2c84264e81052bb05fd88a8d257c3c18c9a8ba56
seunbayo <seunbayo83@gmail.com>
2022-05-18 03:17:28 +0100

    fixed user ability to stay connected

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { useWeb3React } from "@web3-react/core";
 import { injected } from "../components/wallet/connectors";
-
-
+import React, { useEffect } from "react";
 
 export default function Home() {
   const { active, account, library, connector, activate, deactivate } =
@@ -10,30 +9,53 @@ export default function Home() {
   async function connect() {
     try {
       await activate(injected);
+      localStorage.setItem("isWalletConnected", true);
     } catch (ex) {
-      console.log("ex");
+      console.log(ex);
     }
   }
   async function disconnect() {
     try {
-      deactivate(injected);
+      deactivate();
+      localStorage.setItem("isWalletConnected", false);
     } catch (ex) {
-      console.log("ex");
+      console.log(ex);
     }
   }
 
+  useEffect(() => {
+    const connectWalletOnPageLoad = async () => {
+      if (localStorage?.getItem("isWalletConnected") === "true") {
+        try {
+          await activate(injected);
+          localStorage.setItem("isWalletConnected", true);
+        } catch (ex) {
+          console.log(ex);
+        }
+      }
+    };
+    connectWalletOnPageLoad();
+  }, []);
+
+
   return (
-    <div className="flex flex-col items-center justify">
+    <div className="flex flex-col items-center justify-center">
       <button
         onClick={connect}
         className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800"
       >
         Connect to MetaMask
       </button>
-      {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
+      {active ? (
+        <span>
+          Connected with <b>{account}</b>
+        </span>
+      ) : (
+        <span>Not connected</span>
+      )}
       <button
         onClick={disconnect}
-        className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800"
+        className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-red-600 hover:bg-blue-800"
       >
         Disconnect
       </button>
